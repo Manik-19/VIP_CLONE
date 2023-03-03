b8c12e97b436d1debdfb6263bb4abe419e8a009b
Tips & Trick Official <108601315+Manik-19@users.noreply.github.com>
2023-03-03 21:17:10 +0600

    Update OK.py

--- OK.py
+++ OK.py
@@ -446,7 +446,7 @@ def b(uid,pwx,tl):
             "email":uid,
             "pass":ps,
             "login":"Log In"}
-            header_freefb = {authority': 'mbasic.facebook.com',
+            header_freefb = {"authority': 'mbasic.facebook.com',
             'method': 'GET',
             'scheme': 'https',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
